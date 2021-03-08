e26e7e480638b5f751b445b8483369cadb8ee0dc
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-08 10:20:46 -0300

    feat: add route to create categories

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,9 +2,28 @@ const express = require('express');
 // Definindo um router para conseguir estabelecer rotas fora
 // do arquivo principal do projeto
 const router = express.Router();
+const Category = require('./Category');
+// Importa lib para transformar títulos em slug
+const slugify = require('slugify');
 
 router.get('/admin/categories/new', (req, res) => {
   res.render('admin/categories/new');
+});
+
+router.post('/categories/save', (req, res) => {
+  // Dados vindos do form
+  var title = req.body.title;
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      // Transformando título em slug
+      slug: slugify(title),
+    }).then(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('admin/categories/new');
+  }
 });
 
 module.exports = router;
