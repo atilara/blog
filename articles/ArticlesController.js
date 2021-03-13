fa9d532bd0eb560934acbb7b4501771d4e774726
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-13 16:15:27 -0300

    feat: add tinymce to create articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('Artigos');
+router.get('/articles', (req, res) => {
+  res.send('Rota de Artigos');
+});
+
+router.get('/admin/articles/new', (req, res) => {
+  res.render('admin/articles/new');
 });
 
 module.exports = router;
