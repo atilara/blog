5f74b5b3a3cd6ac6c4f8af1c567cc25313b886a5
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-13 16:23:43 -0300

    feat: add selection of categories

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Category = require('../categories/Category');
+
+// Rotas criadas para lidar com artigos
 
 router.get('/articles', (req, res) => {
   res.send('Rota de Artigos');
 });
 
 router.get('/admin/articles/new', (req, res) => {
-  res.render('admin/articles/new');
+  Category.findAll().then((categories) => {
+    // Passando a variável pro view
+    res.render('admin/articles/new', { categories: categories });
+  });
 });
 
 module.exports = router;
