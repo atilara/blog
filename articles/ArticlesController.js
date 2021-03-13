dd74b3b9cf40e115f5def6e775bedd3f8d2a2a86
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-13 16:37:08 -0300

    feat: add creation of articles

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
 // Rotas criadas para lidar com artigos
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
   res.send('Rota de Artigos');
 });
 
@@ -12,6 +14,22 @@ router.get('/admin/articles/new', (req, res) => {
   Category.findAll().then((categories) => {
     // Passando a variável pro view
     res.render('admin/articles/new', { categories: categories });
+  });
+});
+
+router.post('/articles/save', (req, res) => {
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title),
+    body: body,
+    // Campo gerado pelo relacionamento, chave estrangeira
+    categoryId: category,
+  }).then(() => {
+    res.redirect('/admin/articles');
   });
 });
 
--- database/database.js
+++ database/database.js
@@ -3,6 +3,8 @@ const sequelize = require('sequelize');
 const connection = new sequelize('blog', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
+  // Para os campos createdAt e updatedAt sejam preenchidos com os dados da minha
+  timezone: '-03:00',
 });
 
 module.exports = connection;
