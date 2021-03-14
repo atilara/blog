ad3bce8097b1718085831e85723dcb5c8d5c51c6
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-14 16:32:28 -0300

    feat: create page to read articles

--- index.js
+++ index.js
@@ -34,6 +34,22 @@ app.get('/', (req, res) => {
   });
 });
 
+app.get('/:slug', (req, res) => {
+  var slug = req.params.slug;
+  // Procura por um artigo onde o slug for o mesmo que foi passado na URL
+  Article.findOne({ where: { slug: slug } })
+    .then((article) => {
+      if (article != undefined) {
+        res.render('article', { article: article });
+      } else {
+        res.redirect('/');
+      }
+    })
+    .catch((error) => {
+      res.redirect('/');
+    });
+});
+
 app.listen('5500', () => {
   console.log('Server running');
 });
