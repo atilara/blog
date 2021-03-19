6596564a5e71120b69f759b53f4fc2ad82d61cc1
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-19 11:50:12 -0300

    feat: create route to logout

--- user/UsersController.js
+++ user/UsersController.js
@@ -73,4 +73,9 @@ router.post('/authenticate', (req, res) => {
   });
 });
 
+router.get('/logout', (req, res) => {
+  req.session.user = undefined;
+  res.redirect('/login');
+});
+
 module.exports = router;
