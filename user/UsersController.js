7464842b19525627961af89da515dfef80ea4c79
Átila Rodrigues <atila.rodriguesal@gmail.com>
2021-03-18 21:05:32 -0300

    feat: create user in database using bcrypt

--- user/UsersController.js
+++ user/UsersController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+// Package para criptografia
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
   res.send('Listagem de usuárias');
@@ -13,7 +15,20 @@ router.get('/admin/users/create', (req, res) => {
 router.post('/users/create', (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
-  res.json({ email, password });
+
+  var salt = bcrypt.genSaltSync(10);
+  var hash = bcrypt.hashSync(password, salt);
+
+  User.create({
+    email: email,
+    password: hash,
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(() => {
+      res.redirect('/');
+    });
 });
 
 module.exports = router;
